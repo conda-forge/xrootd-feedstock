28f81f02f1f73a439d64bac6d2a88c9e562852fe
Chris Burr <christopher.burr@cern.ch>
2020-12-20 19:14:19 +0100

    Start patching

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,10 @@ cmake ^
     -G "Ninja" ^
     -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -D CMAKE_INSTALL_LIBDIR="lib" ^
     -D CMAKE_BUILD_TYPE=Release ^
+    -D PYTHON_EXECUTABLE="%PYTHON%" ^
+    -D PYTHON_INCLUDE_DIR="%PREFIX%/include" ^
     -S . -B build
 if errorlevel 1 exit 1
 
